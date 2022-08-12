dc04009827a3e60cc9b9c1200286a2f8721a48b0
Lily Tran <lily@Lilys-MBP.lan>
2022-08-12 08:17:54 -0700

    first commit

---
+++ Segue3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Segue3
+//
+//  Created by Lily Tran on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Segue3/Segue3App.swift
@@ -0,0 +1,17 @@
+//
+//  Segue3App.swift
+//  Segue3
+//
+//  Created by Lily Tran on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Segue3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
